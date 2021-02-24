d243a627e195d1549e7436d553015436df2a8170
viravebriyanti30 <vira.vebriyanti2001@gmail.com>
2021-02-24 22:53:07 +0700

    belajarphp

---
+++ belajar.php
@@ -0,0 +1,34 @@
+<?php 
+
+$angka = 1;
+$nama = "vira";
+$angka_desimal = 3.5;
+echo $angka;
+echo "<h2> haiii </h2>";
+echo "<br>";
+echo $nama;
+echo "<br>";
+echo $angka_desimal;
+echo "<br>";
+echo "dokumen saya berada di : ".$_SERVER["DOCUMENT_ROOT"];
+echo "<br>";
+define("PHI", 3.14);
+$jari_jari = 7;
+$luas_lingkaran = PHI * $jari_jari * $jari_jari;
+echo $luas_lingkaran;
+
+ $_nama_lengkap = "vira"; 
+ echo $_nama_lengkap;
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
